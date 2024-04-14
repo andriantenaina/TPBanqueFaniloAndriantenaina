156ff7ffc9da1365e7bcb481409947632ef54581
Fanilo <andriantenainafanilo@gmail.com>
2024-04-14 22:02:11 +0300

    Écriture d'un bean CDI GestionnaireCompte et Bean CDI Init

---
+++ src/main/java/mg/itu/tpbanque/service/GestionnaireCompte.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpbanque.service;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import mg.itu.tpbanque.entity.CompteBancaire;
+
+/**
+ * Façade pour gérer les comptes
+ *
+ * @author Fanilo
+ */
+@DataSourceDefinition(
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        name = "java:app/jdbc/banque",
+        serverName = "localhost",
+        portNumber = 3306,
+        user = "fanilo", // nom et
+        password = "123456", // mot de passe que vous avez donnés lors de la création de la base de données
+        databaseName = "banque",
+        properties = {
+            "useSSL=false",
+            "allowPublicKeyRetrieval=true",
+            "driverClass=com.mysql.cj.jdbc.Driver"
+        }
+)
+@ApplicationScoped
+//@RequestScoped
+public class GestionnaireCompte {
+
+    @PersistenceContext(unitName = "banquePU")
+    private EntityManager em;
+
+    public List<CompteBancaire> getAllComptes() {
+        TypedQuery<CompteBancaire> query = em.createNamedQuery("CompteBancaire.findAll", CompteBancaire.class);
+        return query.getResultList();
+    }
+
+    @Transactional
+    public void creerCompte(CompteBancaire compteBancaire) {
+        em.persist(compteBancaire);
+    }
+
+    public long nbComptes() {
+        TypedQuery<Long> query = em.createNamedQuery("CompteBancaire.count", Long.class);
+        return query.getSingleResult();
+    }
+}
