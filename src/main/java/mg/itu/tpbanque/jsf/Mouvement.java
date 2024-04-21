19b7ec8591a1f0d6e104ab5c2756922b97977839
Fanilo <andriantenainafanilo@gmail.com>
2024-04-21 18:21:54 +0300

    Traitement de l'exception

--- src/main/java/mg/itu/tpbanque/jsf/Mouvement.java
+++ src/main/java/mg/itu/tpbanque/jsf/Mouvement.java
@@ -4,7 +4,6 @@
  */
 package mg.itu.tpbanque.jsf;
 
-
 import mg.itu.tpbanque.service.GestionnaireCompte;
 import java.io.Serializable;
 import jakarta.faces.application.FacesMessage;
@@ -15,6 +14,7 @@ import jakarta.faces.validator.ValidatorException;
 import jakarta.inject.Named;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
+import jakarta.persistence.OptimisticLockException;
 import mg.itu.tpbanque.entity.CompteBancaire;
 import mg.itu.tpbanque.jsf.util.Util;
 
@@ -94,13 +94,20 @@ public class Mouvement implements Serializable {
     }
 
     public String enregistrerMouvement() {
-        if (typeMouvement.equals("ajout")) {
-            gestionnaireCompte.deposer(compte, montant);
-        } else {
-            gestionnaireCompte.retirer(compte, montant);
-        }
-        Util.addFlashInfoMessage("Mouvement enregistré sur compte de " + compte.getNom());
-        return "listeComptes?faces-redirect=true";
+        try {
+            if (typeMouvement.equals("ajout")) {
+                gestionnaireCompte.deposer(compte, montant);
+            } else {
+                gestionnaireCompte.retirer(compte, montant);
+            }
+            Util.addFlashInfoMessage(typeMouvement + " de " + montant
+                    + " enregistré sur compte de " + compte.getNom());
+            return "listeComptes?faces-redirect=true";
+        } catch (OptimisticLockException ex) {
+            Util.messageErreur("Le compte de " + compte.getNom()
+                    + " a été modifié ou supprimé par un autre utilisateur !");
+            return null; // pour rester sur la page s'il y a une exception
+        } // end try
     }
 
 }
